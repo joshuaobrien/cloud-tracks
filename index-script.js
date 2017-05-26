6c8834f0134b8f0f3ff3a1ca47417c6b348a4160
jay-grant <jay.amgrant@gmail.com>
2017-05-27 01:28:29 +1000

    vue2youtooU2

--- index-script.js
+++ index-script.js
@@ -2,12 +2,23 @@ new Vue({
     el: '#content',
     data: {
         message: "cloud tracks",
-        userLink: "https://www.youtube.com/watch?v=rC3L-Z6x4dg"
+        userLink: "https://www.youtube.com/watch?v=rC3L-Z6x4dg",
+        inputStyle: {
+            backgroundColor: '#fafafa'
+        }
     },
 
     methods: {
         pasteLink: function() {
             alert("Pasted")
+        },
+
+        hoverOn: function() {
+            this.inputStyle.backgroundColor = 'white'
+        },
+
+        hoverOff: function() {
+            this.inputStyle.backgroundColor = '#fafafa'
         }
     }
 })
