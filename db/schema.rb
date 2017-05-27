5b5112ab2ecd61504d4789083a2dccefb590b496
Joshua O'Brien <jjobrien12@gmail.com>
2017-05-28 02:37:13 +1000

    Added stray playlist_track file that should have been included with previous commit; added playlist schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527150938) do
+ActiveRecord::Schema.define(version: 20170527163146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20170527150938) do
   create_table "playlist_tracks", force: :cascade do |t|
     t.bigint "playlist_id"
     t.bigint "track_id"
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.bigint "playlist_id"
+    t.bigint "user_id"
+    t.string "name"
   end
 
   create_table "tracks", force: :cascade do |t|
