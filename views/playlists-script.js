e39f114b78fb8c365abbf9180fefab9fc5d18a91
jay-grant <jay.amgrant@gmail.com>
2017-05-27 14:40:06 +1000

    added pause/play skeleton to playlist thumbnails

--- views/playlists-script.js
+++ views/playlists-script.js
@@ -1,17 +1,25 @@
 Vue.component('playlist-item', {
-  // The todo-item component now accepts a
-  // "prop", which is like a custom attribute.
-  // This prop is called todo.
-  props: ['playlist'],
-  template: `<li>
-                <div class="playlist-item">
-                    <div class="playlist-thumbnail-container">
-                            <img src="assets/res/thumbnail-placeholder.jpg">
+    // The todo-item component now accepts a
+    // "prop", which is like a custom attribute.
+    // This prop is called todo.
+    props: ['playlist'],
+
+    template: `<li>
+                    <div class="playlist-item">
+                        <div class="playlist-thumbnail-container">
+                            <a href="#tracks">
+                                <img class="thumbnail" src="assets/res/thumbnail-placeholder.jpg" href="#okay">
+                            </a>
+
+                            <a class="pp-button" href="#play">
+                                <img src="assets/res/play-large.png">
+                            </a>
+                        </div>
+
+                        <p class="playlist-title">{{playlist.title}}</p>
+                        <p class="playlist-artist">{{playlist.artist}}</P>
                     </div>
-                    <p class="playlist-title">{{playlist.title}}</p>
-                    <p class="playlist-artist">{{playlist.artist}}</P>
-                </div>
-            </li>`
+                </li>`
 })
 
 new Vue({
@@ -19,13 +27,14 @@ new Vue({
 
     data: {
         playlists: [
-            {id: 0, title: 'Title One', artist: 'Artist One'},
-            {id: 1, title: 'Title Two', artist: 'Artist Two'},
-            {id: 2, title: 'Title Three', artist: 'Artist Two'},
-            {id: 3, title: 'Title Four', artist: 'Artist Three'},
-            {id: 4, title: 'Title Five', artist: 'Artist Four'},
-            {id: 5, title: 'Title Six', artist: 'Artist Four'},
-            {id: 6, title: 'Title Seven', artist: 'Artist Four'}
-        ]
+            { id: 0, title: 'Title One', artist: 'Artist One' },
+            { id: 1, title: 'Title Two', artist: 'Artist Two' },
+            { id: 2, title: 'Title Three', artist: 'Artist Two' },
+            { id: 3, title: 'Title Four', artist: 'Artist Three' },
+            { id: 4, title: 'Title Five', artist: 'Artist Four' },
+            { id: 5, title: 'Title Six', artist: 'Artist Four' },
+            { id: 6, title: 'Title Seven', artist: 'Artist Four' }
+        ],
+        isHovered: false
     }
 })
