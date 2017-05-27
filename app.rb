65b6dcdf9483afa2c1953e6b71b81fe309b8fb01
Joshua O'Brien <jjobrien12@gmail.com>
2017-05-27 16:02:03 +1000

    test user creation route added

--- app.rb
+++ app.rb
@@ -10,9 +10,16 @@ require './config/environments'
 load 'lib/services/download_service.rb'
 load 'lib/services/splitter_service.rb'
 load 'lib/track.rb'
+load 'lib/user.rb'
 
 downloader = DownloadService.new
 splitter = SplitterService.new
+
+
+#track = Track.create(name: title, artist: artist, duration: duration_in_seconds, filepath: path+"/"+track['title']+".m4a")
+#user = User.create(username: "hey", password: "datstick")
+
+
 
 post '/real/download_tape' do
     content_type :json
@@ -35,4 +42,22 @@ post '/test/download' do
     puts 'Splitting..'
     splitter.split_tape(filename, metadata)
     puts 'Ayy nice'
+end
+
+post '/test/create_user' do
+    unless params[:username].nil? || params[:password].nil?
+
+        user = User.new
+        user.username = params[:username]
+        user.password = params[:password]
+
+        if user.save
+            status 200
+            json 'ok'
+        else
+            status 400
+            json 'not ok'
+        end
+        
+    end
 end
