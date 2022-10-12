7e8786f03af35f4b85cc988090b461fa2633c073
Joshua O'Brien <joshua.obrien@tutanota.com>
2022-10-12 23:35:07 +1100

    we now enter the new era

--- README.md
+++
@@ -1,3 +0,0 @@
-# is anyone there?
-
-# hello
--- lib/user.rb
+++
@@ -1,7 +0,0 @@
-require 'sinatra'
-require 'sinatra/activerecord'
-
-class User < ActiveRecord::Base
-    validates_uniqueness_of :username
-    has_secure_password
-end
