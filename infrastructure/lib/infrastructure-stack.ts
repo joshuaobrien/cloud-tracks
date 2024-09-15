c4f7827d4f1a6f8c4210b0cd9b386681adf5973d
joshuaobrien <joshua.obrien@tutanota.com>
2024-09-15 17:03:42 +1000

    infra: add RDS, allow containers on ECS to access it #14

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,15 +2,16 @@ import * as cdk from 'aws-cdk-lib';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as rds from 'aws-cdk-lib/aws-rds';
 import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
 
-export interface InfrasStackP extends cdk.StackProps {
+export interface InfrastructureStackP extends cdk.StackProps {
   repository: ecr.Repository;
 }
 
 export class InfrastructureStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: InfrasStackP) {
+  constructor(scope: Construct, id: string, props: InfrastructureStackP) {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, 'CloudTracksVPC', {
@@ -21,6 +22,27 @@ export class InfrastructureStack extends cdk.Stack {
       vpc: vpc,
     });
 
+    // Create RDS PostgreSQL Instance
+    const database = new rds.DatabaseInstance(this, 'PostgresDB', {
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_16_4,
+      }),
+      vpc: vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      publiclyAccessible: false,
+      multiAz: false,
+      allocatedStorage: 20,
+      storageType: rds.StorageType.GP2,
+      credentials: rds.Credentials.fromGeneratedSecret('postgresUser'),
+      databaseName: 'CloudTracksDB',
+      backupRetention: cdk.Duration.days(7),
+      deleteAutomatedBackups: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
     const taskDefinition = new ecs.FargateTaskDefinition(this, 'CloudTracksTaskDef', {
       memoryLimitMiB: 512,
       cpu: 256,
@@ -29,6 +51,15 @@ export class InfrastructureStack extends cdk.Stack {
     const container = taskDefinition.addContainer('CloudTracksContainer', {
       image: ecs.ContainerImage.fromEcrRepository(props.repository),
       logging: ecs.LogDriver.awsLogs({ streamPrefix: 'CloudTracks' }),
+      environment: {
+        DATABASE_HOST: database.dbInstanceEndpointAddress,
+        DATABASE_PORT: database.dbInstanceEndpointPort,
+        DATABASE_NAME: 'CloudTracksDB',
+        DATABASE_USER: 'postgresUser',
+      },
+      secrets: {
+        DATABASE_PASSWORD: ecs.Secret.fromSecretsManager(database.secret!, 'password'),
+      },
     });
 
     container.addPortMappings({
@@ -43,12 +74,8 @@ export class InfrastructureStack extends cdk.Stack {
       publicLoadBalancer: true,
     });
 
-    fargateService.targetGroup.configureHealthCheck({
-      path: "/health",
-      interval: cdk.Duration.seconds(30),
-      timeout: cdk.Duration.seconds(5),
-      healthyHttpCodes: "200",
-    });
+    // Allow ECS Task (via its Security Group) to connect to RDS
+    database.connections.allowDefaultPortFrom(fargateService.service.connections);
 
     // Optional: Define Auto Scaling policies
     const scaling = fargateService.service.autoScaleTaskCount({
